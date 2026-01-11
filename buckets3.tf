961702a5df27bac05383f4a95999d7274a2702ae
2rspoo <rodrigo.spoo@gmail.com>
2026-01-11 13:38:30 -0300

    Ajustando app

--- buckets3.tf
+++ buckets3.tf
@@ -1,4 +1,11 @@
-data "aws_s3_bucket" "buckets3-eks" {
+resource "aws_s3_bucket" "buckets3-eks" {
   bucket = var.bucket_name_backend
-
+  key    = var.bucket_key_backend # O nome do arquivo
+  region = var.region
+  tags = var.tags_prod
+  force_destroy = true
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = [bucket]
+  }
 }
